0180bcd3efbb9e422c5b674e029312954425d1df
programmerfaysal <programmerfaysal@gmail.com>
2023-02-24 14:33:51 +0600

    first commit

--- js/custom.js
+++ js/custom.js
@@ -32,23 +32,6 @@ document.addEventListener("DOMContentLoaded", function(){
 // DOMContentLoaded  end
 
 
-//Sticky Header Js Code
-document.addEventListener("DOMContentLoaded", function(){
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 50) {
-          document.getElementById('navbar_top').classList.add('fixed-top');
-          // add padding top to show content behind navbar
-          navbar_height = document.querySelector('.navbar').offsetHeight;
-          document.body.style.paddingTop = navbar_height + 'px';
-        } else {
-          document.getElementById('navbar_top').classList.remove('fixed-top');
-           // remove padding top from body
-          document.body.style.paddingTop = '0';
-        } 
-    });
-  });
-
-
   //table
   $(document).ready(function () {
     $('#table').DataTable();
